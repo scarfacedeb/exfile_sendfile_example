45c050b83e4075f6ba0f5fe3749f97fe2e4fd095
scarfacedeb <scarfacedeb@gmail.com>
2019-06-18 17:11:39 +0300

    Initial commit

---
+++ lib/exfile_sendfile.ex
@@ -0,0 +1,18 @@
+defmodule ExfileSendfile do
+  @moduledoc """
+  Documentation for ExfileSendfile.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExfileSendfile.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
