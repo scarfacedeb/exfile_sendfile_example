96da54df4c521e091d9095c74582e752eac3b8c7
scarfacedeb <scarfacedeb@gmail.com>
2019-09-12 20:22:07 +0300

    Add port_control trace
    Why:
    
    To have a concrete proof that it's not getting called.

--- lib/exfile_sendfile/application.ex
+++ lib/exfile_sendfile/application.ex
@@ -1,5 +1,6 @@
 defmodule ExfileSendfile.Application do
   use Application
+  use Tracer
 
   def start(_type, _args) do
     children = [
@@ -8,6 +9,12 @@ defmodule ExfileSendfile.Application do
       ExfileSendfile.Temp
     ]
     opts = [strategy: :one_for_one, name: ExfileSendfile.Supervisor]
+
+    start_trace()
     Supervisor.start_link(children, opts)
+  end
+
+  def start_trace do
+    run Display, process: :all,  max_tracing_time: :timer.minutes(10), match: global :erlang.port_control(port, 66167597, args)
   end
 end
